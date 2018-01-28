ca36f259e6baa39e8cd167c245fa9469779f7fc2
Sarah Fletcher <sfletch@mit.edu>
2018-01-28 16:06:34 -0500

    Edited megan's files to run on slurm - first try

--- BMA_code/MomUni_P.R
+++ BMA_code/MomUni_P.R
@@ -1,18 +1,39 @@
-setwd("~/Desktop/Research/BMA/REA_Bayes")
+
+# Set working directory: two options depending whether on svante
+if (exists("SLURM_NTASKS_PER_NODE", mode="environment")) {
+  setwd("~/net/fs02/d2/sfletch/Mombasa_climate/BMA_code")
+} else {
+  setwd("~/Documents/MATLAB/Mombasa_Climate/BMA_code")
+}
 
 source("./REA.Gibbs.r")
-#source("./REA.GM.r")
-#source("./REAMV.GM.r")
 install.packages("foreach")
 library(foreach)
 
+# Need to use this package for the dopar commands to work
+install.packages("doParallel")
+library(doParallel)
+
 #[1] 22 21
 args(REA.Gibbs)
-#function(X, X0, lambda0, Y, N= 15, save.every=50, burn.in= 100*
-#save.every)
-setwd("~/Desktop/Research/BMA/")
-tmp = read.csv("lambda0.csv",header = FALSE)
+tmp = read.csv("Input/lambda0.csv",header = FALSE)
 lambda0 = as.matrix(tmp)
+
+# Set up the parallelization. If running on slurm, use the number of cores available in the job. If running on desktop, use 2. 
+if (exists("SLURM_NTASKS_PER_NODE", mode="environment")) {
+  registerDoParallel(cores=(Sys.getenv("SLURM_NTASKS_PER_NODE")))
+} else {
+  registerDoParallel(cores=2)
+}
+
+# If running on svante, get jobid for name of file when saving
+if (exists("SLURM_JOB_ID", mode="environment")) {
+  jobid = Sys.getenv("SLURM_JOB_ID")
+} else {
+  jobid = ""
+}
+# Get date for file save name
+currentDate = Sys.Date()
 
 #  Here we could think about running this in parallel
 year = c(1990,2000,2010,2020,2030,2040,2050,2060,2070,2080,2090)
@@ -20,31 +41,20 @@ foreach (scen_ii = 1:2) %dopar%{
   foreach (ii = 1:10) %dopar%{
     yearX = year[ii]
     yearY = year[ii+1]
-    str1 = sprintf("X_%d.csv",yearX)
-    str2 = sprintf("X_%d.csv",yearY)
-    str3 = sprintf("output%f.csv",yearY)
+    str1 = sprintf("Input/X_%d.csv",yearX)
+    str2 = sprintf("Input/X_%d.csv",yearY)
     tmp = read.csv(str1,header = FALSE)
     X = as.matrix(tmp)
     
     tmp = read.csv(str2,header = FALSE)
     Y = as.matrix(tmp)
     
-    #tmp = read.csv("X0TU.csv",header = FALSE)
-    #X0T = as.matrix(tmp[c(ii),c(scen_ii)])
-    
-    #REA.Gibbs(X[c(1),],X0T[c(1)],lambda0[c(1)],Y[c(1),],N=1000)->rg0
-    
-    #mu0str = sprintf("muUT_output%d_scen%d.csv",yearY,scen_ii)
-    #nu0str = sprintf("nuUT_output%d_scen%d.csv",yearY,scen_ii)
-    #write.table(rg0$mu, file = mu0str,row.names=FALSE, col.names=FALSE)
-    #write.table(rg0$nu, file = nu0str,row.names=FALSE, col.names=FALSE)
-    
-    tmp = read.csv("X0PU.csv",header = FALSE)
+    tmp = read.csv("Input/X0PU.csv",header = FALSE)
     X0P = tmp[c(ii),c(scen_ii)]
     REA.Gibbs(X[c(2),],X0P[c(1)],lambda0[c(2)],Y[c(2),],N=1000)->rg0
     
-    mu0str = sprintf("muUP_output%d_scen%d.csv",yearY,scen_ii)
-    nu0str = sprintf("nuUP_output%d_scen%d.csv",yearY,scen_ii)
+    mu0str = paste(sprintf("Output/muUP_%d_scen%d",yearY,scen_ii),"job", jobid, currentDate, ".csv", sep="_")
+    nu0str = paste(sprintf("Output/nuUP_%d_scen%d",yearY,scen_ii),"job", jobid, currentDate, ".csv", sep="_")
     write.table(rg0$mu, file = mu0str,row.names=FALSE, col.names=FALSE)
     write.table(rg0$nu, file = nu0str,row.names=FALSE, col.names=FALSE)
   }
---
+++ BMA_code/pre_process_BMA.m
@@ -0,0 +1,54 @@
+load('Input/Mombasa_TandP.mat'); 
+rewrite_XYlambda = true;
+create_scens = true;
+reading_Routput = 0;
+create_timeseries = 0;
+
+%% Creating initial X, Y and lambda values.  These don't change 
+if rewrite_XYlambda
+    for year = 70:100
+        YT0(year-69) = mean(T0(12*(year-1)+1:12*year));
+        YP0(year-69) = log(mean(P0(12*(year-1)+1:12*year)));
+    end
+
+    X0 = [mean(YT0), mean(YP0)]';
+    SD = [std(YT0), std(YP0)]';
+    lambda0 = SD.^(-2);
+
+    for year = 1:200
+        YTij(year,:) = mean(Tij(12*(year-1)+1:12*year,:),1);
+        YPij(year,:) = log(mean(Pij(12*(year-1)+1:12*year,:),1));
+    end
+
+    % This is when we use 1970-2000 to predict 2070-2100
+    %X = [mean(YTij(70:100,:),1)', mean(YPij(70:100,:),1)']';
+    %Y = [mean(YTij(FYi:FYi+30,:),1)', mean(YPij(FYi:FYi+30,:),1)']';
+
+    % Saving variables for R code: 
+    csvwrite('Input/lambda0.csv',lambda0)
+    
+    for decade = 1:11
+        X = [mean(YTij(10*(decade-1)+80:10*(decade-1)+100,:),1)', mean(YPij(10*(decade-1)+80:10*(decade-1)+100,:),1)']';
+        str1 = sprintf('Input/X_%2.0f.csv',1990+10*(decade-1));
+        csvwrite(str1,X)
+        %csvwrite(str2,Y)
+    end
+end
+
+%% Creating scenarios
+if create_scens
+    deltaT_X0(1:10,1) = zeros(10,1);
+    deltaT_X0(1:10,2) = 0:0.25:0.25*9;
+    deltaT_X0(1:10,3) = 0:0.5:4.5;
+
+    deltaP_X0(1:10,1) = 0:-0.08:-0.72;
+    deltaP_X0(1:10,2) = zeros(10,1);
+    deltaP_X0(1:10,3) = 0:0.08:0.72;
+
+    % Univariate case
+    X0_Topts = X0(1) + deltaT_X0;
+    X0_Popts = log(exp(X0(2))+exp(X0(2))*deltaP_X0);
+
+    csvwrite('Input/X0TU.csv',X0_Topts)
+    csvwrite('Input/X0PU.csv',X0_Popts) 
+end
