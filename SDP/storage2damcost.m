77a6b22414323ed2dbdbc1b32424d12b0817df4b
Sarah Fletcher <sfletch@mit.edu>
2018-03-17 15:49:28 -0400

    switched back to old dam cost bc didn't seem like new one was ever picking flex

--- SDP/storage2damcost.m
+++ SDP/storage2damcost.m
@@ -11,16 +11,19 @@ index = find(costmodel.storage == storage);
 dcost = costmodel.dam_cost(index);
 
 if flex_storage
-    index = find(costmodel.storage == flex_storage);
-    dcost_large = costmodel.dam_cost(index);
-    dcost_flex = (dcost + dcost_large)/2;
-    ecost = (dcost_large - dcost_flex) * 1.5
-    dcost = dcost_flex;
     
-%     added_storage = flex_storage - storage;
-%     indexFlex = find(costmodel.storage == flex_storage);
-%     ecost = added_storage * costmodel.unit_cost(indexFlex)*1.5;
-%     dcost = dcost;
+%     % upfront proactive placeholder
+%     index = find(costmodel.storage == flex_storage);
+%     dcost_large = costmodel.dam_cost(index);
+%     dcost_flex = (dcost + dcost_large)/2;
+%     ecost = (dcost_large - dcost_flex) * 1.5
+%     dcost = dcost_flex;
+    
+    added_storage = flex_storage - storage;
+    indexFlex = find(costmodel.storage == flex_storage);
+    ecost = added_storage * costmodel.unit_cost(indexFlex)*1.5;
+    dcost = dcost;
+    
 end
 
 end
